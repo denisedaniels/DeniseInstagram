393030cc3052ead8232797fb64b1e9bf846d7b9e
denisedaniels <denise_daniels@bloomfield.edu>
2020-07-10 23:00:03 -0400

    Implemented Login

--- app/src/main/java/com/example/deniseinstagram/LoginActivity.java
+++ app/src/main/java/com/example/deniseinstagram/LoginActivity.java
@@ -1,11 +1,19 @@
 package com.example.deniseinstagram;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+//import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -18,6 +26,11 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        //Check if the user is already logged in
+        if (ParseUser.getCurrentUser()!=null) {
+            goMainActivity();
+        }
 
         //Get all id references for login
         etUsername= findViewById(R.id.etUsername);
@@ -39,6 +52,27 @@ public class LoginActivity extends AppCompatActivity {
 
     private void loginUser(String username, String password){
         Log.i(TAG, "Attempting to login user" +username);
-        //Navigate to main activity
+        //Execute login on background thread
+        ParseUser.logInInBackground(username, password, new LogInCallback() {
+            @Override
+            public void done(ParseUser user, ParseException e) {
+                if(e !=null) {
+                    Log.e(TAG, "Login Issue", e);
+                    Toast.makeText(LoginActivity.this,"Issue with login",Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                //If login is successful navigate to main activity
+                goMainActivity();
+                //Add toast to indicate that login was successful
+                Toast.makeText(LoginActivity.this,"Success!",Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    // Use intent system to navigate to the new activity
+    private void goMainActivity() {
+        Intent i=new Intent (this, MainActivity.class);
+        startActivity(i);
+        finish();
     }
 }
--- app/src/main/java/com/example/deniseinstagram/MainActivity.java
+++ app/src/main/java/com/example/deniseinstagram/MainActivity.java
@@ -1,13 +1,30 @@
 package com.example.deniseinstagram;
 
-import android.support.v7.app.AppCompatActivity;
+//import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG="MainActivity";
+    private EditText etDescription;
+    private Button btnCaptureImage;
+    private ImageView ivPostImage;
+    private Button btnSubmit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etDescription= findViewById(R.id.etDescription);
+        btnCaptureImage= findViewById(R.id.btnCaptureImage);
+        ivPostImage= findViewById(R.id.ivPostImage);
+        btnSubmit= findViewById(R.id.btnSubmit);
+
     }
 }
