c8f1d4da3da50ec492f54898ef525d3ca844a084
denisedaniels <denise_daniels@bloomfield.edu>
2020-07-10 23:39:05 -0400

    Defined Data Object & Query

--- app/src/main/java/com/example/deniseinstagram/MainActivity.java
+++ app/src/main/java/com/example/deniseinstagram/MainActivity.java
@@ -2,11 +2,22 @@ package com.example.deniseinstagram;
 
 //import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,5 +37,60 @@ public class MainActivity extends AppCompatActivity {
         ivPostImage= findViewById(R.id.ivPostImage);
         btnSubmit= findViewById(R.id.btnSubmit);
 
+        //queryPosts();
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String description= etDescription.getText().toString();
+                if(description.isEmpty()){
+                    Toast.makeText(MainActivity.this,"Description can not be empty",Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                ParseUser currentUser= ParseUser.getCurrentUser();
+                savePost(description,currentUser);
+            }
+        });
+
+    }
+
+    private void savePost(String description, ParseUser currentUser) {
+        Post post= new Post();
+        post.setDescription(description);
+        //post.setImage();
+        post.setUser(currentUser);
+        post.saveInBackground(new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                if(e!=null){
+                    Log.e(TAG, "Error while saving", e);
+                    Toast.makeText(MainActivity.this,"Error while saving",Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                Log.i(TAG, "Post save was successful");
+                etDescription.setText("");
+            }
+        });
+        
+    }
+
+    private void queryPosts() {
+        ParseQuery<Post> query = ParseQuery.getQuery(Post.class);
+        query.include(Post.KEY_USER);
+        query.findInBackground(new FindCallback<Post>() {
+            @Override
+            public void done(List<Post> posts, ParseException e) {
+                if(e!=null){
+                    Log.e(TAG, "Error getting posts", e);
+                    Toast.makeText(MainActivity.this,"Error getting posts",Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                for(Post post: posts){
+                    //Logs information from all posts
+                    Log.i(TAG, "Post" +post.getDescription()+ ", username: " +post.getUser().getUsername());
+                }
+
+            }
+        });
     }
 }
